dd9c11dc9bccf0ec99a5e4cd8183ca524da45411
eternnoir <eternnoir@gmail.com>
2015-04-01 10:05:07 +0800

    Add url checker.

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	maxPagePtr := flag.Int("m", 99999, "Max Pages to Download")
 	debugPtr := flag.Bool("d", false, "Enable debug mode.")
 	flag.Parse()
+	if len(*urlPtr) < 1 {
+		fmt.Println("Url can not be empty.Use -h to get some help.")
+		return
+	}
 	fmt.Println("Start download photo from:" + *urlPtr)
 	fmt.Println("The max number of download page is " + strconv.Itoa(*maxPagePtr))
 
