6294edc0deacf31049836748935c99a2769055f0
eternnoir <eternnoir@gmail.com>
2015-04-13 11:08:01 +0800

    Add image size support.

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	pathPtr := flag.String("p", "/tmp", "Download path.")
 	maxPagePtr := flag.Int("m", 99999, "Max Pages to Download")
 	debugPtr := flag.Bool("d", false, "Enable debug mode.")
+	sizePtr := flag.String("s", "o", "What image size you want to download.'o' is means origin.'l' means large.")
 	flag.Parse()
 	if len(*urlPtr) < 1 {
 		fmt.Println("Url can not be empty.Use -h to get some help.")
@@ -19,7 +20,8 @@ func main() {
 	}
 	fmt.Println("Start download photo from:" + *urlPtr)
 	fmt.Println("The max number of download page is " + strconv.Itoa(*maxPagePtr))
+	fmt.Println("Download image size is " + *sizePtr)
 
 	downloader := flickrdownloader.InitDownloader(*debugPtr)
-	downloader.SaveAllPhoto(*urlPtr, *pathPtr, *maxPagePtr)
+	downloader.SaveAllPhoto(*urlPtr, *pathPtr, *maxPagePtr, *sizePtr)
 }
